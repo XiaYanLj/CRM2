102f57ff39066050c4b71216de778fb6e8e54f9c
XiaYanLj <739107259@qq.com>
2020-08-31 20:46:52 +0800

    登陆完成

---
+++ js/axios.defaults.js
@@ -0,0 +1,36 @@
+//对axios进行二次封装
+axios.defaults.baseURL = "http://localhost:8888";
+// 数据以表单的形式扔给服务器
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+//仍然使用表单的形式扔给服务器，数据格式改变
+axios.defaults.transformRequest = function (data) {
+    if(!data) return data;
+    let result = '';
+    for(let attr in data){
+        if(!data.hasOwnProperty(attr)) break;
+        result += `&${attr}=${data[attr]}`
+    }
+    return result.substring(1);
+};
+//配置请求拦截器
+axios.interceptors.request.use(config =>{
+    return config
+})
+//配置响应拦截器
+axios.interceptors.response.use(response=>{
+    return response.data;
+},reason=>{
+    //如果路径出错，返回404 和其他错误...
+    // console.dir(reason);
+    if(reason.response){
+        switch(String(reason.responce.status)){
+            case "404":
+                alert("当前请求的地址不存在")
+                break;
+            default:
+                break;
+        }
+    }
+    //直接创建一个失败的Promise
+    return Promise.reject(reason);
+})
---
+++ js/login.js
@@ -0,0 +1,32 @@
+$(function(){
+ //登录功能
+    $(".submit").click(async function(e){
+        let account = $(".userName").val().trim();
+        let password = $(".userPass").val().trim();
+        
+        if(account === "" || password === ""){
+            alert("账号和密码不能为空~");
+            return;
+        }
+        // 自己设置正则校验规则
+        password = md5(password);
+        //发出ajax请求
+        // axios.post("/user/login",{
+        //     account,
+        //     password
+        // }).then(res=>{
+        //     console.log(res);
+        // }).catch(err=>{
+        //     console.log(err);
+        // })
+        let res = await axios.post("/user/login",{account,password})
+        // console.log(res);
+
+        if(parseInt(res.code) === 0){
+            alert("登录成功")
+            window.location.href="index.html"
+            return;
+        }
+        alert("用户名和密码出错了")
+    })
+})
